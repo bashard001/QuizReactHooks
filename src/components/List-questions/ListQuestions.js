34f7e5ad6e47c339faa28bf2509e819981cb2688
bashard001 <bdaseh@gmail.com>
2021-09-24 12:35:27 -0700

    initial files

---
+++ src/components/List-questions/ListQuestions.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function ListQuestions() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
---
+++ src/components/header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./header.styles.scss"
+
+export default function Header() {
+    return (
+        <div className="header">
+            <div id="timer">
+
+            </div>
+            <div id="timeleft"></div>
+        </div>
+    )
+}
