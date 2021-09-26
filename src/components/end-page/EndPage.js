269b5f81a97421ee9b6773530631b9f82fa1945b
bashard001 <bdaseh@gmail.com>
2021-09-26 12:44:10 -0700

    handle submit

--- src/components/end-page/EndPage.js
+++ src/components/end-page/EndPage.js
@@ -1,11 +1,16 @@
-import React,{ useContext } from 'react'
+import React,{ useContext, useState } from 'react'
 import DispatchContext from '../context/DispatchContext'
 
 import "./end.styles.scss"
 
 export default function EndPage() {
-
+    const [initial, setInitial] = useState("")
     const { start } = useContext(DispatchContext)
+
+    function handleSubmit (e){
+        e.preventDefault()
+        console.log(initial)
+    }
 
     if(!start.end){
         return null
@@ -14,9 +19,9 @@ export default function EndPage() {
     return (
         <div>
             <div>You have successfully finished the Quiz</div>
-            <form>
-                <input type="text" placeholder="Enter your initials" maxLength="2" required/>
-                <button>Click to get your score</button>
+            <form onSubmit={handleSubmit}>
+                <input type="text" placeholder="Enter your initials" maxLength="2" onChange={(e) => setInitial(e.target.value)} required/>
+                <button type="submit">Click to get your score</button>
             </form>
             
         </div>
