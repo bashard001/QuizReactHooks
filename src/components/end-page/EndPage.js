5b06c60867af8489a91ae1797135611c9b55a692
bashard001 <bdaseh@gmail.com>
2021-09-27 13:15:43 -0700

    display which questions were answered right or wrong

--- src/components/end-page/EndPage.js
+++ src/components/end-page/EndPage.js
@@ -7,13 +7,20 @@ export default function EndPage() {
     const [initial, setInitial] = useState("")
     const [showRes, setShowRes] = useState(false)
     const { start, progress } = useContext(DispatchContext)
+    const highestScore = localStorage.getItem("initials")
+    const pastScore = localStorage.getItem("score")
 
     function handleSubmit(e) {
         e.preventDefault()
         console.log(initial)
+        if (!pastScore) {
+            localStorage.setItem("initials", `${initial.toUpperCase()} : ${progress.score}`)
+            localStorage.setItem("score", progress.score)
+        } else if (pastScore && pastScore < progress.score) {
+            localStorage.setItem("initials", `${initial.toUpperCase()} : ${progress.score}`)
+            localStorage.setItem("score", progress.score)
+        }
 
-        localStorage.setItem("initials", `${initial} : ${progress.score}`)
-        localStorage.setItem("score", progress.score)
         setShowRes(true)
     }
 
@@ -25,15 +32,20 @@ export default function EndPage() {
         <div>
             <div>You have successfully finished the Quiz</div>
 
-            {showRes ? <fieldset>
-                <legend>Scores</legend>
-                <h4>Highest Score: <br /> BD ; 2</h4>
-                <h3>Your Score : </h3>
-                <hr />
-            </fieldset> : <form onSubmit={handleSubmit}>
-                <input type="text" placeholder="Enter your initials" maxLength="2" onChange={(e) => setInitial(e.target.value)} required />
-                <button type="submit">Click to get your score</button>
-            </form>}
+            {showRes ?
+                <fieldset>
+                    <legend>Scores</legend>
+                    <h4>Highest Score: <br /> {highestScore}</h4>
+
+                    <h3>Your Score : {progress.score} out of 5</h3>
+                    <hr />
+                    {progress.answers.map((answer, index) => <div key={index}>Q {index + 1} : {answer.answer} Answer</div>)}
+                </fieldset>
+                :
+                <form onSubmit={handleSubmit}>
+                    <input type="text" placeholder="Enter your initials" maxLength="2" onChange={(e) => setInitial(e.target.value)} required />
+                    <button type="submit">Click to get your score</button>
+                </form>}
         </div>
     )
 }
